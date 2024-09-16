ca2073ceab825949535ffe65837c0ebbf66aacbf
joaquinaedo <jedo@udesa.edu.ar>
2024-09-16 15:02:56 -0300

    emprolijando

---
+++ OneDrive/Documentos/UDESA/2doano/segundosemestre/programacion3/ti/prog3-grupo5-edo-ramos-vaccaro/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+const Header =  () => {
+    return(
+        <nav> 
+            <a href = "/"> <img src = "./img/logo.png"></img></a>
+            <ul>
+                <li>Home</li>
+                <li>Favoritos</li>
+                <li>Ver Más</li>
+            </ul>
+        </nav>
+    ) 
+}
+
+export default Header; 
